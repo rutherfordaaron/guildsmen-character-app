ba3102343c811cd19479f761b0366b3ba49c685e
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-18 13:47:51 -0700

    Made it friendly on bigger screens

--- src/CharacterSheet.js
+++ src/CharacterSheet.js
@@ -197,7 +197,7 @@ const CharacterSheet = (props) => {
   const primaryCheck = (e) => {
     let skill = {};
     for (let i = 0; i < character.skills.length; i++) {
-      if (character.skills[i].name == e.target.value) {
+      if (character.skills[i].name === e.target.value) {
         skill = character.skills[i];
       }
     }
@@ -247,7 +247,7 @@ const CharacterSheet = (props) => {
   const secondaryCheck = (e) => {
     let skill = {};
     for (let i = 0; i < character.skills.length; i++) {
-      if (character.skills[i].name == e.target.value) {
+      if (character.skills[i].name === e.target.value) {
         skill = character.skills[i];
       }
     }
@@ -754,14 +754,24 @@ const CharacterSheet = (props) => {
 
       <div className="section details">
         <h2>Character Details</h2>
-        <h3>Goals & Motives</h3>
-        <textarea value={character.details[0].content} className="detailsInput" name="goals" onChange={detailsChange} />
-        <h3>Personal Morals</h3>
-        <textarea value={character.details[1].content} className="detailsInput" name="morals" onChange={detailsChange} />
-        <h3>Flaws & Weaknesses</h3>
-        <textarea value={character.details[2].content} className="detailsInput" name="weaknesses" onChange={detailsChange} />
-        <h3>Important Connections</h3>
-        <textarea value={character.details[3].content} className="detailsInput" name="connections" onChange={detailsChange} />
+        <div className="detailsGrid">
+          <div>
+            <h3>Goals & Motives</h3>
+            <textarea value={character.details[0].content} className="detailsInput" name="goals" onChange={detailsChange} />
+          </div>
+          <div>
+            <h3>Personal Morals</h3>
+            <textarea value={character.details[1].content} className="detailsInput" name="morals" onChange={detailsChange} />
+          </div>
+          <div>
+            <h3>Flaws & Weaknesses</h3>
+            <textarea value={character.details[2].content} className="detailsInput" name="weaknesses" onChange={detailsChange} />
+          </div>
+          <div>
+            <h3>Important Connections</h3>
+            <textarea value={character.details[3].content} className="detailsInput" name="connections" onChange={detailsChange} />
+          </div>
+        </div>
       </div>
 
       <div className={`dead ${character.dead ? "" : "hidden"}`}>
