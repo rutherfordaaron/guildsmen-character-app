0b169c792534b0dc963e2fda450252a6c2c34884
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-19 15:15:57 -0700

    Created skills component
    - Moved the Skills section to it's own component

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -7,6 +7,7 @@ import Stats from "./characterSheet/Stats";
 import Luck from "./characterSheet/Luck";
 import Harm from "./characterSheet/Harm";
 import Experience from "./characterSheet/Experience";
+import Skills from "./characterSheet/Skills";
 
 const CharacterSheet = (props) => {
   let characters = JSON.parse(localStorage.getItem('guildsmenCharacters'));
@@ -128,147 +129,7 @@ const CharacterSheet = (props) => {
     setMessage(<div />);
   }
 
-  const skillCheck = (e) => {
-    let skill = {};
-    for (let i = 0; i < character.skills.length; i++) {
-      if (character.skills[i].name === e) {
-        skill = character.skills[i];
-      }
-    }
 
-    let rolls = rollDice();
-    let modifier = skill.modifier;
-    let modifierString;
-    let name = skill.name;
-    let total = rolls.num1 + rolls.num2 + modifier;
-
-    if (modifier > -1) {
-      modifierString = `+${modifier}`;
-    }
-
-    if (total >= 8) {
-      let newCharacter = { ...character };
-      newCharacter.experienceProgress++;
-
-      if (newCharacter.experienceProgress >= 4) {
-        newCharacter.experience++;
-        newCharacter.experienceProgress = 0;
-      }
-
-      setCharacter(newCharacter);
-    }
-
-    setMessage(
-      <div className='message'>
-        <p className="messageHead"><strong>{name} Check!</strong></p>
-        <p>You rolled {rolls.num1} and {rolls.num2}.</p>
-        <p>Your modifier is {modifierString || modifier}</p>
-        <p className="messageTotal"><strong>Total: {total}</strong></p>
-      </div>
-    )
-
-    return (rolls.num1 + rolls.num2 + modifier)
-  }
-
-  const primaryCheck = (e) => {
-    let skill = {};
-    for (let i = 0; i < character.skills.length; i++) {
-      if (character.skills[i].name === e.target.value) {
-        skill = character.skills[i];
-      }
-    }
-
-    if (!skill.spec1) {
-      setDiceState('');
-      setMessage(
-        <div className='message'>
-          <p className="messageHead"><strong>No primary specialty for {skill.name}!</strong></p>
-          <p>A primary specialty is unlocked with 5 EXP, but requires a +2 in this skill. </p>
-        </div>
-      )
-    } else {
-      let rolls = rollDice();
-      let modifier = skill.modifier + 2;
-      let modifierString;
-      let name = skill.name;
-      let total = rolls.num1 + rolls.num2 + modifier;
-
-      if (modifier > -1) {
-        modifierString = `+${modifier}`;
-      }
-
-      if (total >= 8) {
-        let newCharacter = { ...character };
-        newCharacter.experienceProgress++;
-
-        if (newCharacter.experienceProgress > 4) {
-          newCharacter.experience++;
-          newCharacter.experienceProgress = 0;
-        }
-
-        setCharacter(newCharacter);
-      }
-
-      setMessage(
-        <div className='message'>
-          <p className="messageHead"><strong>{name} Check!</strong></p>
-          <p>You rolled {rolls.num1} and {rolls.num2}.</p>
-          <p>Your modifier is {modifierString || modifier}</p>
-          <p className="messageTotal"><strong>Total: {total}</strong></p>
-        </div>
-      )
-    }
-  }
-
-  const secondaryCheck = (e) => {
-    let skill = {};
-    for (let i = 0; i < character.skills.length; i++) {
-      if (character.skills[i].name === e.target.value) {
-        skill = character.skills[i];
-      }
-    }
-
-    if (!skill.spec2) {
-      setDiceState('');
-      setMessage(
-        <div className='message'>
-          <p className="messageHead"><strong>No secondary specialty for {skill.name}!</strong></p>
-          <p>A secondary specialty is unlocked with 4 EXP, but requires that this skill have a primary specialty.</p>
-        </div>
-      )
-    } else {
-      let rolls = rollDice();
-      let modifier = skill.modifier + 1;
-      let modifierString;
-      let name = skill.name;
-      let total = rolls.num1 + rolls.num2 + modifier;
-
-      if (modifier > -1) {
-        modifierString = `+${modifier}`;
-      }
-
-      if (total >= 8) {
-        let newCharacter = { ...character };
-        newCharacter.experienceProgress++;
-
-        if (newCharacter.experienceProgress > 4) {
-          newCharacter.experience++;
-          newCharacter.experienceProgress = 0;
-        }
-
-        setCharacter(newCharacter);
-      }
-
-      setMessage(
-        <div className='message'>
-          <p className="messageHead"><strong>{name} Check!</strong></p>
-          <p>You rolled {rolls.num1} and {rolls.num2}.</p>
-          <p>Your modifier is {modifierString || modifier}</p>
-          <p className="messageTotal"><strong>Total: {total}</strong></p>
-        </div>
-      )
-    }
-  }
 
   const addMyth = () => {
     let newCharacter = { ...character };
@@ -432,53 +293,13 @@ const CharacterSheet = (props) => {
         <Experience character={character} setCharacter={setCharacter} expStore={expStore} setExpStore={setExpStore} />
       </div>
 
-      <div className="section skills">
-        <h2>Skills</h2>
-        <div className="skillsGrid">
-          {character.skills.map((el, i) => {
-            return (
-              <div key={`stat${i}`} className={`skillContainer ${el.name === 'Throwdown' ? 'throwdown' : 'skill'}`}>
-                <div className="labelContainer">
-                  <button type='button' className='diceButton'>
-                    <input type='image' value={el.name} onClick={() => { skillCheck(el.name) }} src='/static/icons/dice-solid.svg' alt={`roll for ${el.name}`} className='filter' />
-                  </button>
-                  <p><em>{el.name}</em></p>
-                </div>
-
-                <div className="statModifiers">
-                  <p>-1</p>
-                  <p>+0</p>
-                  <p>+1</p>
-                  <p>+2</p>
-                  <p>+3</p>
-                </div>
-                <div className="bubbleContainer">
-                  <div className='bubble filled'></div>
-                  <div className={el.modifier >= 0 ? 'bubble filled' : "bubble"}></div>
-                  <div className={el.modifier >= 1 ? 'bubble filled' : "bubble"}></div>
-                  <div className={el.modifier >= 2 ? 'bubble filled' : "bubble"}></div>
-                  <div className={el.modifier >= 3 ? 'bubble filled' : "bubble"}></div>
-                </div>
-
-                <div className="specialtyContainer">
-                  <div className="specialty">
-                    <button type='button' className='diceButton'>
-                      <input type='image' value={el.name} onClick={primaryCheck} src='/static/icons/dice-solid.svg' alt={`roll for ${el.name}`} className='filter' />
-                    </button>
-                    <p>{el.spec1 || 'Primary'} (+2)</p>
-                  </div>
-                  <div className="specialty">
-                    <button type='button' className='diceButton'>
-                      <input type='image' value={el.name} onClick={secondaryCheck} src='/static/icons/dice-solid.svg' alt={`roll for ${el.name}`} className='filter' />
-                    </button>
-                    <p>{el.spec2 || 'Secondary'} (+1)</p>
-                  </div>
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      <Skills
+        character={character}
+        setCharacter={setCharacter}
+        setMessage={setMessage}
+        rollDice={rollDice}
+        setDiceState={setDiceState}
+      />
 
       <div className="section addiction">
         <h2>Myth Addiction</h2>
