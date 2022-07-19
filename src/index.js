10f51ad4e74815ee7b37a4a597835c540e550e00
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-19 09:22:42 -0700

    Updated import statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import CharacterSheet from './CharacterSheet';
+import CharacterSheet from './components/CharacterSheet';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
