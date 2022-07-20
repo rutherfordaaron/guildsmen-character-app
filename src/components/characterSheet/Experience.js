131ca96d0b035e01d3ef11244fa01fa8bfae7a88
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-19 17:36:42 -0700

    Added comments
    - Explain my functions
    - Explain my JSX

--- src/components/characterSheet/Experience.js
+++ src/components/characterSheet/Experience.js
@@ -1,6 +1,9 @@
 import ExpStore from "../ExpStore";
 
 const Experience = (props) => {
+  /**
+   * Show the experience store by mounting the ExpStore component
+   */
   const startExpStore = () => {
     props.setExpStore(
       <ExpStore
@@ -11,6 +14,9 @@ const Experience = (props) => {
     );
   }
 
+  /**
+   * Hide the experience store by unmounting the ExpStore component
+   */
   const closeExpStore = () => {
     props.setExpStore(<div />);
   }
@@ -19,15 +25,18 @@ const Experience = (props) => {
   return (
     <div className="section experience">
       <h2>Experience</h2>
+      {/* Container for experience progress bubbles */}
       <div className="bubbleContainer">
         <div className={props.character.experienceProgress >= 1 ? "filled bubble" : "bubble"} />
         <div className={props.character.experienceProgress >= 2 ? "filled bubble" : "bubble"} />
         <div className={props.character.experienceProgress >= 3 ? "filled bubble" : "bubble"} />
         <div className={props.character.experienceProgress >= 4 ? "filled bubble" : "bubble"} />
       </div>
+      {/* Container for experience points */}
       <div className="experiencePoints">
         <p><strong>{props.character.experience}</strong></p>
       </div>
+      {/* Button to show ExpStore */}
       <button type="button" onClick={startExpStore} className="expBtn">Spend EXP</button>
       {props.expStore}
     </div>
