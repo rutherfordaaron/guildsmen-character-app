d47ed64de2f078d2cd869ca24fea4b070a3fc0e3
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-19 17:28:31 -0700

    Added comments
    - Explain functions and  how they work
    - Improve code understandability

--- src/components/characterSheet/Addiction.js
+++ src/components/characterSheet/Addiction.js
@@ -1,6 +1,12 @@
 const Addiction = (props) => {
+  // Addiction progress value array for use in mapping out addiction progress, since addiction progress is one block value
   const addArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
+  /**
+   * If you've never had addiction above 0 before, it jumps you addictionProgress to 3 and addiction level to 1, then sets everAddicted to true so it doesn't happen again
+   * If your Myth uses are at 6 or higher, kills your character and sets/displays a death message
+   * Otherwise, your need is set to 0, myth uses increments, addiction progress is increment, and addiction is recalculated.
+   */
   const addMyth = () => {
     let newCharacter = { ...props.character };
     if (!newCharacter.everAddicted) {
@@ -23,6 +29,7 @@ const Addiction = (props) => {
     props.setCharacter(newCharacter);
   }
 
+  // Decrement mythUses without letting mythUses fall below 0
   const minusMyth = () => {
     let newCharacter = { ...props.character };
     if (newCharacter.mythUses > 0) {
@@ -37,6 +44,7 @@ const Addiction = (props) => {
       <p>Level</p>
       <div className="addLevel">
         <p>0</p>
+        {/* Map out numbers for addiction levels */}
         {addArr.map((el, i) => {
           if (el % 3 === 0) {
             return (
@@ -46,6 +54,7 @@ const Addiction = (props) => {
         })}
       </div>
       <div className="addMeter">
+        {/* Map out boxes for addiction progress meter */}
         {addArr.map((el, i) => {
           return (
             <div
@@ -57,6 +66,7 @@ const Addiction = (props) => {
       </div>
       <p>Need</p>
       <div className="needContainer">
+        {/* Map number of bubbles equal to max need ((addictLevel - 8) * 1 + 2) */}
         {addArr.map((el, i) => {
           let val = el / 3;
           if (el % 3 === 0) {
@@ -70,6 +80,7 @@ const Addiction = (props) => {
         })}
       </div>
       <p>Myth Uses</p>
+      {/* Buttons to increment and decrement mythUses */}
       <div className="plusMinus">
         <button type='button' className='diceButton'>
           <input type='image' onClick={minusMyth} src='/static/icons/circle-minus-solid.svg' alt='subtract harm' className='filter' />
@@ -78,6 +89,7 @@ const Addiction = (props) => {
           <input type='image' onClick={addMyth} src='/static/icons/circle-plus-solid.svg' alt='add harm' className='filter' />
         </button>
       </div>
+      {/* Myth uses */}
       <p>{props.character.mythUses}</p>
     </div>
   )
